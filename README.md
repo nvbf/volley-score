dfb8b123d759fd2e8434c498b0fc95bf52e7fdf8
Sindre Svendby <sinsvend@gmail.com>
2017-01-08 13:52:25 +0100

    inital commit

---
+++ README.md
@@ -0,0 +1,21 @@
+scoreboard-obs-plugin
+========================
+
+creates 2 files (`score1.txt` and `score2.txt`), with the points from the website.
+
+by default you can find the application on localhost:3000 if you do not set PORT
+
+## Prerequisites
+Node
+
+
+## Env var
+
+OBSPLUGINPATH (obligatory) : The path to where it should update the point
+
+PORT (optional) : Will default to 3000 if it's not set
+
+
+## E.g
+
+`OBSPLUGINPATH=/home/dir/obs/ PORT=80 node index.js`
---
+++ index.js
@@ -0,0 +1,25 @@
+var fs = require('fs');
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+
+if(!process.env.OBSPLUGINPATH) {
+  throw error new Error(' OBSPLUGINPATH need to be defined');
+}
+
+app.post('/update', jsonParser, (req, res) => {
+  fs.writeFile(OBSPLUGINPATH + '/score1.txt', req.body.a);
+  fs.writeFile(OBSPLUGINPATH + '/score2.txt', req.body.b);
+  res.send('mottat')
+})
+
+app.use(express.static('public'))
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('app listening on port ' + (process.env.PORT || 3000))
+})
