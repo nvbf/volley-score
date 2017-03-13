1f40ffbf3df50b1e748604e27a8c975f3d0068cc
Thor Even Tutturen <t.tutturen@gmail.com>
2017-03-13 11:03:57 +0100

    add redis

--- index.js
+++ index.js
@@ -2,34 +2,26 @@ var fs = require('fs');
 var express = require('express');
 var app = express();
 var Promise = require('bluebird');
-var readFile = Promise.promisify(fs.readFile);
+const Redis = require('./src/cache/redis');
 
 var bodyParser = require('body-parser');
 
 // create application/json parser
-var jsonParser = bodyParser.json()
-
-
-if(!process.env.OBSPLUGINPATH) {
-  throw new Error('OBSPLUGINPATH needs to be defined');
-}
-
-var getPath = fileName => process.env.OBSPLUGINPATH + fileName;
-var getFile = name => readFile(getPath(name), 'utf-8');
+var jsonParser = bodyParser.json();
 
 function update(pointsA = 0, pointsB = 0, setA = 0,
   setB = 0, nameA = '', nameB = '', logoA = '', logoB = '') {
-  fs.writeFile(process.env.OBSPLUGINPATH + '/scoreA.txt', pointsA);
-  fs.writeFile(process.env.OBSPLUGINPATH + '/scoreB.txt', pointsB);
-  fs.writeFile(process.env.OBSPLUGINPATH + '/setA.txt', setA);
-  fs.writeFile(process.env.OBSPLUGINPATH + '/setB.txt', setB);
-  fs.writeFile(process.env.OBSPLUGINPATH + '/nameA.txt', nameA);
-  fs.writeFile(process.env.OBSPLUGINPATH + '/nameB.txt', nameB);
-  fs.writeFile(process.env.OBSPLUGINPATH + '/logoA.txt', logoA);
-  fs.writeFile(process.env.OBSPLUGINPATH + '/logoB.txt', logoB);
+  Redis.set('pointsA', pointsA);
+  Redis.set('pointsB', pointsB);
+  Redis.set('setA', setA);
+  Redis.set('setB', setB);
+  Redis.set('nameA', nameA);
+  Redis.set('nameB', nameB);
+  Redis.set('logoA', logoA);
+  Redis.set('logoB', logoB);
 }
 
-// Init files empty when starting server, to make sure files are there
+// Init redis empty when starting server, to make redis is filled
 update();
 
 app.post('/update', jsonParser, (req, res) => {
@@ -48,14 +40,14 @@ app.post('/update', jsonParser, (req, res) => {
 
 app.get('/scores', (req, res) => {
   Promise.all([
-    getFile('/scoreA.txt'),
-    getFile('/scoreB.txt'),
-    getFile('/setA.txt'),
-    getFile('/setB.txt'),
-    getFile('/nameA.txt'),
-    getFile('/nameB.txt'),
-    getFile('/logoA.txt'),
-    getFile('/logoB.txt'),
+    Redis.get('pointsA'),
+    Redis.get('pointsB'),
+    Redis.get('setA'),
+    Redis.get('setB'),
+    Redis.get('nameA'),
+    Redis.get('nameB'),
+    Redis.get('logoA'),
+    Redis.get('logoB')
   ]).then(data => {
     res.json({
       scoreA: data[0],
---
+++ src/cache/redis.js
@@ -0,0 +1,39 @@
+const ioredis = require('ioredis');
+
+class Redis {
+
+  constructor() {
+    this.client = ioredis(process.env.REDIS_URL || 'redis://127.0.0.1:6379');
+  }
+
+  get(key) {
+    return this.client.get(key).then(value => JSON.parse(value));
+  }
+
+  getAll() {
+    return this.client.keys('*');
+  }
+
+  addSet(key, values) {
+    return this.client.sadd(key, values);
+  }
+
+  getSet(key) {
+    return this.client.smembers(key);
+  }
+
+  set(key, value) {
+    return this.client.set(key, JSON.stringify(value));
+  }
+
+  setExpired(key, expirationInMilliseconds, value) {
+    return this.client.set(key, JSON.stringify(value), 'px', expirationInMilliseconds);
+  }
+
+  purge() {
+    return this.client.flushall();
+  }
+
+}
+
+module.exports = new Redis();
