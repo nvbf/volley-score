cc525e575b4b561e670599ae83f7a15376fed0fb
Thor Even Tutturen <thor@bustbyte.no>
2018-05-18 11:38:10 +0200

    add mutations for changing logo and color visibility

--- components/settings/index.js
+++ components/settings/index.js
@@ -5,6 +5,8 @@ import ToggleBox from '../shared/ToggleBox';
 import { SectionTitle, SubSectionTitle } from '../shared/Title';
 import SectionContainer from '../shared/SectionContainer';
 import SelectBox from '../shared/SelectBox';
+import gql from 'graphql-tag';
+import { Query, Mutation } from 'react-apollo';
 
 const SectionGroup = styled.div`
   display: flex;
@@ -12,36 +14,100 @@ const SectionGroup = styled.div`
   width: 100%;
 `;
 
+const GET_SETTINGS = gql`
+  query GetMatchSettings($matchId: String) {
+    localScoreboard(id: $matchId) {
+      showLogo
+      showColor
+      homeTeam {
+        name
+        logo
+        color
+      }
+      guestTeam {
+        name
+        logo
+        color
+      }
+    }
+  }
+`;
+
+const SET_COLOR_VISIBILITY = gql`
+  mutation SetColorVisibility($id: String, $show: Boolean) {
+    setColorVisibility(matchId: $id, show: $show) {
+      showColors
+    }
+  }
+`;
+
+const SET_LOGO_VISIBILITY = gql`
+  mutation SetLogoVisibility($id: String, $show: Boolean) {
+    setLogoVisibility(matchId: $id, show: $show) {
+      showLogos
+    }
+  }
+`;
+
 function Settings(props) {
-  const { showColors, showLogos, homeTeam, guestTeam } = props;
+  console.log(props);
   return (
-    <React.Fragment>
-      <SectionTitle>Settings</SectionTitle>
-      <SectionContainer>
-        <ToggleBox
-          onChange={val => console.log(val.target.checked)}
-          checked={showColors}
-          label="Show shirt colors"
-        />
-        <ToggleBox
-          onChange={val => console.log(val.target.checked)}
-          checked={showLogos}
-          label="Show logos"
-        />
-      </SectionContainer>
-      <SectionContainer>
-        <SectionGroup>
-          <SubSectionTitle>Home Team</SubSectionTitle>
-          <SelectBox text={homeTeam.name} logo={homeTeam.logo} selectText="Select club" />
-          <SelectBox text={homeTeam.color} color={homeTeam.color} selectText="Select color" />
-        </SectionGroup>
-        <SectionGroup>
-          <SubSectionTitle>Guest Team</SubSectionTitle>
-          <SelectBox text={guestTeam.name} logo={guestTeam.logo} selectText="Select club" />
-          <SelectBox text={guestTeam.color} color={guestTeam.color} selectText="Select color" />
-        </SectionGroup>
-      </SectionContainer>
-    </React.Fragment>
+    <Query query={GET_SETTINGS} variables={{ matchId: props.matchId }}>
+      {({ loading, error, data }) => {
+        if (loading) return 'Loading...';
+        if (error) return 'Error...';
+        const { showColors, showLogos, homeTeam, guestTeam } = data;
+        return (
+          <React.Fragment>
+            <SectionTitle>Settings</SectionTitle>
+            <SectionContainer>
+              <Mutation mutation={SET_COLOR_VISIBILITY}>
+                {setColorVisibility => (
+                  <ToggleBox
+                    onChange={val =>
+                      setColorVisibility({
+                        variables: { id: props.matchId, show: val.target.checked },
+                      })
+                    }
+                    checked={showColors}
+                    label="Show shirt colors"
+                  />
+                )}
+              </Mutation>
+              <Mutation mutation={SET_LOGO_VISIBILITY}>
+                {setLogoVisibility => (
+                  <ToggleBox
+                    onChange={val =>
+                      setLogoVisibility({
+                        variables: { id: props.matchId, show: val.target.checked },
+                      })
+                    }
+                    checked={showLogos}
+                    label="Show logos"
+                  />
+                )}
+              </Mutation>
+            </SectionContainer>
+            <SectionContainer>
+              <SectionGroup>
+                <SubSectionTitle>Home Team</SubSectionTitle>
+                <SelectBox text={homeTeam.name} logo={homeTeam.logo} selectText="Select club" />
+                <SelectBox text={homeTeam.color} color={homeTeam.color} selectText="Select color" />
+              </SectionGroup>
+              <SectionGroup>
+                <SubSectionTitle>Guest Team</SubSectionTitle>
+                <SelectBox text={guestTeam.name} logo={guestTeam.logo} selectText="Select club" />
+                <SelectBox
+                  text={guestTeam.color}
+                  color={guestTeam.color}
+                  selectText="Select color"
+                />
+              </SectionGroup>
+            </SectionContainer>
+          </React.Fragment>
+        );
+      }}
+    </Query>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -7,17 +7,34 @@ import Settings from '../components/settings';
 import ScoreControl from '../components/scoreControl';
 import Footer from '../components/footer';
 import withData from '../src/apollo/withData';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const GET_MATCH_ID = gql`
+  query GetMatchID {
+    matchId @client
+  }
+`;
 
 function ScorePage() {
   return (
-    <PageContainer>
-      <PreTitle>Volleystream.no</PreTitle>
-      <Title>Volley Score</Title>
-      <MatchId onChange={console.log} />
-      <Settings showColors={false} showLogos />
-      <ScoreControl />
-      <Footer>Laget av Thor Even Tutturen, NTNUI Volleyball</Footer>
-    </PageContainer>
+    <Query query={GET_MATCH_ID}>
+      {({ loading, error, data }) => {
+        if (loading) return 'Loading...';
+        if (error) return 'Error...';
+        const { matchId } = data;
+        return (
+          <PageContainer>
+            <PreTitle>Volleystream.no</PreTitle>
+            <Title>Volley Score</Title>
+            <MatchId onChange={console.log} />
+            <Settings matchId={matchId} />
+            <ScoreControl />
+            <Footer>Laget av Thor Even Tutturen, NTNUI Volleyball</Footer>
+          </PageContainer>
+        );
+      }}
+    </Query>
   );
 }
 
