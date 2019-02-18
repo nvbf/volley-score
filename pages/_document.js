b769e600adff82713b4c5322e44f6d50995fa78a
Sindre Øye Svendby <sinsvend@gmail.com>
2019-02-18 10:52:58 +0100

    Fix render issue

--- pages/_document.js
+++ pages/_document.js
@@ -22,6 +22,7 @@ export default class MyDocument extends Document {
         </Head>
         <body>
           <div className="root">{main}</div>
+          <NextScript />
         </body>
       </html>
     );
