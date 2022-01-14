aac2eb65ec316114167a06d3af47013f3fa2ffb4
oveien <oveien@gmail.com>
2022-01-14 07:28:19 +0100

    Add support for score delay (#44)
    Using scoreDelay GET attribute.

--- store/store.js
+++ store/store.js
@@ -31,6 +31,10 @@ class ScoreStore {
     this.matchId = matchId;
   }
 
+  setScoreDelay(scoreDelay) {
+    this.scoreDelay = scoreDelay;
+  }
+
   @action
   startUpdates = () => {
     this.intervalId = setInterval(this.update, 2000);
@@ -42,25 +46,34 @@ class ScoreStore {
     clearInterval(this.intervalId);
   };
 
+  doUpdateData = (data) => {
+    this.homeTeam = {
+      points: data.pointsA || 0,
+      sets: data.setA || 0,
+      logo: data.logoA || '',
+      name: data.nameA || '',
+      color: data.colorA || '',
+    };
+    this.awayTeam = {
+      points: data.pointsB || 0,
+      sets: data.setB || 0,
+      logo: data.logoB || '',
+      name: data.nameB || '',
+      color: data.colorB || '',
+    };
+    this.showLogos = data.showLogos;
+    this.showColors = data.showColors;
+    this.isShowing = data.isShowing;
+  }
+
   update = () => {
     axios.get(`/api/scores/${this.matchId}`).then(({ data }) => {
-      this.homeTeam = {
-        points: data.pointsA || 0,
-        sets: data.setA || 0,
-        logo: data.logoA || '',
-        name: data.nameA || '',
-        color: data.colorA || '',
-      };
-      this.awayTeam = {
-        points: data.pointsB || 0,
-        sets: data.setB || 0,
-        logo: data.logoB || '',
-        name: data.nameB || '',
-        color: data.colorB || '',
-      };
-      this.showLogos = data.showLogos;
-      this.showColors = data.showColors;
-      this.isShowing = data.isShowing;
+      if (this.scoreDelay) {
+        setTimeout(() => this.doUpdateData(data), this.scoreDelay * 1000);
+      }
+      else {
+        this.doUpdateData(data);
+      }
     });
   };
 }
